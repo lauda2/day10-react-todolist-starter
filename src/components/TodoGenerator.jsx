eed630b061d0072edeee21d7179e788bfcb23c36
Danny Lau <lauda2@oocl.com>
2025-09-15 14:38:05 +0800

    feat: add data to server

--- src/components/TodoGenerator.jsx
+++ src/components/TodoGenerator.jsx
@@ -1,18 +1,29 @@
 import { useContext, useState } from 'react';
 import TodoContext from "../contexts/TodoContext";
+import { api } from '../api/mockApi';
 
 function TodoGenerator() {
-    const { state, dispatch } = useContext(TodoContext);
+    const { dispatch } = useContext(TodoContext);
     const [text, setText] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text.trim()) {
+            api.post("/todos", { text: text, done: false })
+                .then(response => {
+                    dispatch({ type: "ADD_TODO", payload: response.data });
+                    setText("");
+                })
+                .catch(error => {
+                    console.error("Error adding todo:", error);
+                });
+        }
+    };
 
     return (<div>
         <input className="add-todo-input" type="text" onChange={(e) => {
             setText(e.target.value);
         }} value={text} />
-        <button className="add-button" onClick={() => {
-            dispatch({ type: "ADD_TODO", payload: { id: state.length + 1, text: text, done: false } });
-            setText("");
-        }}>add</button>
+        <button className="add-button" onClick={handleSubmit}>add</button>
     </div>);
 }
 
