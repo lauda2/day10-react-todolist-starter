76693453260c473914cf77c2a310f7efa8c083c0
Danny Lau <lauda2@oocl.com>
2025-09-15 10:58:01 +0800

    feat: TodoDetailPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useReducer } from 'react';
+import { useReducer, useContext } from 'react';
 import './App.css';
 import TodoContext from './contexts/TodoContext';
 import TodoReducer from './reducers/TodoReducer';
-import { createBrowserRouter, NavLink, Outlet, RouterProvider, } from 'react-router';
+import { createBrowserRouter, NavLink, Outlet, RouterProvider, useParams } from 'react-router';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
+import ToDoItem from './components/TodoItem';
 
 function DefaultLayout() {
   return (
@@ -15,6 +16,9 @@ function DefaultLayout() {
             <li>
               <NavLink to="/">Home</NavLink>
             </li>
+            <li>
+              <NavLink to="/todos/1">ID 1</NavLink>
+            </li>
           </ul>
         </nav>
       </header>
@@ -23,6 +27,13 @@ function DefaultLayout() {
       </main>
     </div>
   );
+}
+
+function TodoDetailPage() {
+  const { id } = useParams();
+  const { state } = useContext(TodoContext);
+  const todo = state.find(item => item.id == id);
+  return <div>{todo ? <ToDoItem todo={todo} /> : "Todo not found"}</div>;
 }
 
 
@@ -35,6 +46,10 @@ const routes = createBrowserRouter([
       {
         path: "/",
         element: <HomePage />
+      },
+      {
+        path: "/todos/:id",
+        element: <TodoDetailPage />
       }
     ]
   }
