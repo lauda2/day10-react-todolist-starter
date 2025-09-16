f6a9315aa07f4b7c30014d2b434477cb3d0a7814
Danny Lau <lauda2@oocl.com>
2025-09-16 15:53:18 +0800

    feat: integrate with backend

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,7 +1,24 @@
+import { message } from 'antd';
 import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: "https://68c7ac9e5d8d9f51473288db.mockapi.io/",
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/",
   headers: { "Content-Type": "application/json" },
   timeout: 10000
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const { status } = error.response;
+    if (status === 404) {
+      message.error(error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export { api }
